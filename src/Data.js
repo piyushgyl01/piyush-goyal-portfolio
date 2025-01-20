5ace53eff46dd62bff6a0828ec3818312ef08bf2
piyush.gyl <piyush2022ug@gmail.com>
2025-01-20 19:50:11 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -8,7 +8,7 @@ export const projects = [
     users' product-related questions."`,
     type: ["Product Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041212564213780/Frame_1000002483.png?ex=676827ae&is=6766d62e&hm=af43e5fde61146c0fa73ab098039a7cc4b47b623a6b1045153a63d41d04bb8c4&=&width=1050&height=700",
+      "https://github.com/piyushgyl01/images/blob/main/customerSupport.png?raw=true",
     websiteUrl:
       "https://www.figma.com/design/ex0UCTxbz3SNf0OkaeUG6I/Customer-Support-Page-By-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
     sourceCodeUrl: "https://github.com/username/design-system",
@@ -17,13 +17,13 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Blog Page Design",
+    title: "Work Showcase Design",
     description: `"I designed and animated Design Studios' work
     showcase page, starting with an eye-catching hero
     section to immediately grab visitors' attention."`,
     type: ["Product Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041451144613909/Frame_1000002489.png?ex=676827e7&is=6766d667&hm=023e8bcef83a43aff096dcea706e603a04ba84e7f4974788e47ca1696911cd27&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images/blob/main/showcase.png?raw=true",
     websiteUrl:
       "https://www.figma.com/design/dgubJXu8P1gKA8vu3fklpa/Blog-Page-by-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
     sourceCodeUrl: "https://github.com/username/design-system",
@@ -38,7 +38,7 @@ export const projects = [
     generation."`,
     type: ["Product Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041450821521408/Frame_1000002488.png?ex=676827e7&is=6766d667&hm=e2a9664ad073b7dd22391dde0ff46e6876b5b21cc33ba1aa9f033cd7ea9e8c14&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/pocketPod.png?raw=true",
     websiteUrl:
       "https://www.figma.com/design/017fjZWUhVM8Nugs5a22Ws/PiyushGoyal's-Assignment-Three?node-id=51-567&p=f&t=kNxygiKmyfplXpaw-0",
     sourceCodeUrl: "https://github.com/username/design-system",
@@ -65,7 +65,7 @@ export const projects = [
     description: `"I animated motion graphics for socials to promote client's products"`,
     type: ["Motion Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041450033119242/Frame_1000002486.png?ex=676827e6&is=6766d666&hm=d5b6a84b43fcf0aedd677feb51f514d1dd07d47a924cdb4e9a8c0bac21064de4&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002485.png?raw=true",
     websiteUrl: "https://youtube.com/shorts/GMYxY7NeECE",
     sourceCodeUrl: "https://github.com/username/design-dev-project",
     technologies: ["Motion Design", "After Effects", "Figma"],
@@ -92,7 +92,7 @@ export const projects = [
     to enhance user experience and storytelling."`,
     type: ["Motion Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041450343235605/Frame_1000002487.png?ex=676827e6&is=6766d666&hm=24b32d5c5bc29adc7bc0730258d5438f5e47eb3627a987aa0ce807c0c3b50c44&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002487.png?raw=true",
     websiteUrl: "https://yobee.co.in/publisher",
     sourceCodeUrl: "https://github.com/username/design-system",
     technologies: ["Motion Design", "After Effects", "Figma"],
@@ -122,7 +122,7 @@ export const projects = [
     "About Us page, explaining its mission and services."`,
     type: ["Motion Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041449349185627/Frame_1000002484.png?ex=676827e6&is=6766d666&hm=81085425ce00b1816edc7664e2f51583faa681fb9bb9749367ba25d2dea18eec&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/what.png?raw=true",
     websiteUrl: "https://youtu.be/1PuQBU_T4B8",
     sourceCodeUrl: "https://github.com/username/design-dev-project",
     technologies: ["Motion Design", "After Effects", "Figma"],
@@ -146,7 +146,7 @@ export const projects = [
     description: `"I animated motion graphics for a rebranding announcement"`,
     type: ["Motion Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320062370638205069/Frame_1000004535.png?ex=67683b62&is=6766e9e2&hm=aceeb1f77bf78fb3eef4d4c1b4a53c3931b7f6b7dc745ea6a088beffc67448ac&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/r.png?raw=true",
     websiteUrl: "https://youtube.com/shorts/MNvswBrXung",
     sourceCodeUrl: "https://github.com/username/design-dev-project",
     technologies: ["Motion Design", "After Effects", "Figma"],
@@ -158,7 +158,7 @@ export const projects = [
     description: `"I animated motion graphics for socials to promote client's products"`,
     type: ["Motion Design"],
     image:
-      "https://media.discordapp.net/attachments/1156145270971715626/1320041449668083722/Frame_1000002485.png?ex=676827e6&is=6766d666&hm=166596f131c71198d2668d72f0e0c71b102e845082650414023efb06d3e77451&=&width=1200&height=800",
+      "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002486.png?raw=true",
     websiteUrl: "https://www.youtube.com/shorts/0C99eO1643c",
     sourceCodeUrl: "https://github.com/username/design-dev-project",
     technologies: ["Motion Design", "After Effects", "Figma"],
